0f169c390ce285a3fdc2ddfb05dada3c6b017fe7
Brasley Joaquim <joaquimbrasley@gmail.com>
2024-03-24 20:35:23 +0000

    chore:split information between different variables

--- element.sh
+++ element.sh
@@ -6,9 +6,14 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 if [[ $1 ]]
 then
-  ELEMENT= $PSQL "SELECT elements.atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements FULL JOIN properties ON elements.atomic_number=properties.atomic_number JOIN types ON properties.type_id = types.type_id  WHERE elements.atomic_number=$1 OR name='$1' OR symbol='$1'"
-  echo $ELEMENT
-  echo "TEST"
+  ELEMENT=$($PSQL "SELECT elements.atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements FULL JOIN properties ON elements.atomic_number=properties.atomic_number JOIN types ON properties.type_id = types.type_id  WHERE elements.atomic_number=$1 OR name='$1' OR symbol='$1'")
+  if [[ -z "$ELEMENT" ]]
+  then
+    echo "I could not find that element in the database."
+  else
+     IFS='|' read ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT <<< $ELEMENT
+      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+  fi
 else 
   echo "Please provide an element as an argument."
 fi
