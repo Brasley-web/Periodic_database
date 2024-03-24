cbb41fcde390d5cf600de9048c714b9d7f9187d0
Brasley Joaquim <joaquimbrasley@gmail.com>
2024-03-24 19:23:07 +0000

    test:querying elements in database

--- element.sh
+++ element.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-P
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 
 
 if [[ $1 ]]
 then
+  ELEMENT= $PSQL "SELECT elements.atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements FULL JOIN properties ON elements.atomic_number=properties.atomic_number JOIN types ON properties.type_id = types.type_id  WHERE elements.atomic_number=$1 OR name='$1' OR symbol='$1'"
+  echo $ELEMENT
   echo "TEST"
 else 
   echo "Please provide an element as an argument."
